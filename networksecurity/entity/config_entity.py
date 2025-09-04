d2b6b6412c5f5ea21ccff3d9537084289c1f7ce1
Ritik Rawat <75121026+ritikrawat2447@users.noreply.github.com>
2025-09-04 17:46:26 +0530

    Data Validation Completed

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -7,7 +7,7 @@ print(training_pipeline.ARTIFACT_DIR)
 
 class TrainingPipelineConfig:
     def __init__(self,timestamp=datetime.now()):
-        timestamp=timestamp.strftime("%m%_%d_%Y_%H_%M_%S")
+        timestamp=timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name = training_pipeline.PIPELINE_NAME
         self.artifact_name = training_pipeline.ARTIFACT_DIR
         self.artifact_dir = os.path.join(self.artifact_name,timestamp)
