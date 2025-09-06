28bd511589b6f00e88b7a383e0d8bd57121a0626
Ritik Rawat <75121026+ritikrawat2447@users.noreply.github.com>
2025-09-06 23:09:26 +0530

    Pipeline Complete

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -35,5 +36,12 @@ if __name__=='__main__':
         logging.info("Data Transformation completed")
         print(data_transformation_artifact)
 
+        ## Model Training
+        logging.info("Model Training Started")
+        model_training_config = ModelTrainerConfig(training_pipeline_config)
+        model_traing = ModelTrainer(model_training_config,data_transformation_artifact)
+        model_training_artifact = model_traing.initiate_model_trainer()
+        logging.info("Model Training artifact created")
+ 
     except Exception as e :
         raise NetworkSecurityException(e,sys)
